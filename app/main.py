6d5c5ead9d3cf21fa52e98965b3162cc1c28b076
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2026-09-05 00:37:56 +0300

    add webhook endpoint for service now

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI , status
+from .models import IncidentPayload
 # for debugging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,3 +11,8 @@ app = FastAPI(title="Helpy ServiceNow Agent")
 def health():
     return {"status": "it's workingg!!"} # to check everything is fine
 
+# wevhook endpoint post for service now 
+@app.post("/webhook", status_code=status.HTTP_202_ACCEPTED)
+def webhook(payload: IncidentPayload):
+    logger.info("Received %s (sys_id=%s)", payload.number, payload.incident_sys_id)
+    return {"accepted": True}
