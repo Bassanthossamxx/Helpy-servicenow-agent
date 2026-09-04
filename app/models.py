6751ac717b99203ec0d504be944358ee4a229b8f
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2026-09-05 00:09:03 +0300

    add incident payload model

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+
+class IncidentPayload(BaseModel):
+    incident_sys_id: str
+    number: str
+    short_description: str = Field(max_length=160) # small desc 
+    description: str = "" # can be empty 
+    priority: int = Field(ge=1, le=5) # from json 1 to 5
+    
